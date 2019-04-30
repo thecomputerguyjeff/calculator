b9aa57446872056db67287901377cd74fd5cc469
Jeff Mayer <TheComputerGuy.Jeff@gmail.com>
2019-04-29 19:35:53 -0500

    add headers

--- src/main/java/com/ti/calculator/CalculatorController.java
+++ src/main/java/com/ti/calculator/CalculatorController.java
@@ -1,6 +1,9 @@
 package com.ti.calculator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +17,10 @@ public class CalculatorController {
     private CalculatorService calculatorService;
 
     @GetMapping(value = "/add/{x}/{y}")
-    public Integer add(@PathVariable Integer x, @PathVariable Integer y) {
-
-        return calculatorService.add(x,y);
-
+    public Object add(@PathVariable Integer x, @PathVariable Integer y) {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+        return ResponseEntity.ok().headers(httpHeaders).body(calculatorService.add(x,y));
     }
 
     @GetMapping(value = "/subtract/{x}/{y}")
